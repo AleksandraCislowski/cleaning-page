23320dcc641d0072d61ff23ace41193a905311fe
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-09 22:49:18 +0100

    section1

---
+++ app/[lng]/components/section1/section1.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import me from "../../../../assets/me.jpg";
+import { useTranslation } from "../../../i18n";
+import {
+  PersonYearCounter,
+  FirmaYearCounter,
+} from "../../components/year-counter";
+import classes from "./section1.module.css";
+
+export default async function SectionOne({ lng }) {
+  const { t } = await useTranslation(lng);
+  return (
+    <main className={classes.mainbox}>
+      <div className={classes.box}>
+        <Image src={me} className={classes.img} />
+      </div>
+      <div className={classes.box}>
+        <h1>{t("title")}</h1>
+        <p>
+          {t("par1")}
+          <PersonYearCounter />
+          {t("par2")}
+          <FirmaYearCounter />
+          {t("par3")}
+        </p>
+      </div>
+      <div className={classes.box}>Karuzela</div>
+    </main>
+  );
+}
--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -1,24 +1,13 @@
-import { useTranslation } from "../i18n";
-import { FirmaYearCounter } from "./components/year-counter";
-import { PersonYearCounter } from "./components/year-counter";
 import Hero from "./components/hero/hero";
 import classes from "./components/navigation/navigation.module.css";
+import SectionOne from "./components/section1/section1";
 
-export default async function Home({ params: { lng } }) {
-  const { t } = await useTranslation(lng);
-
+export default function Home({ lng }) {
   return (
     <main>
       <Hero />
       <div className={classes.bg2} />
-      <h1>{t("title")}</h1>
-      <p>
-        {t("par1")}
-        <PersonYearCounter />
-        {t("par2")}
-        <FirmaYearCounter />
-        {t("par3")}
-      </p>
+      <SectionOne lng={lng} />
     </main>
   );
 }
