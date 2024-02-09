213129980ffec31d8e7898ce53dc4df61cdcc33a
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-09 21:19:29 +0100

    hero

---
+++ app/[lng]/components/hero/hero.js
@@ -0,0 +1,13 @@
+import classes from "./hero.module.css";
+
+export default function Hero() {
+  return (
+    <div className={classes.hero}>
+      <h1>
+        Aleksandra <br />
+        Cislowski <br />
+        Städfirma
+      </h1>
+    </div>
+  );
+}
--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -1,12 +1,14 @@
 import { useTranslation } from "../i18n";
 import { FirmaYearCounter } from "./components/year-counter";
 import { PersonYearCounter } from "./components/year-counter";
+import Hero from "./components/hero/hero";
 
 export default async function Home({ params: { lng } }) {
   const { t } = await useTranslation(lng);
 
   return (
     <main>
+      <Hero />
       <h1>{t("title")}</h1>
       <p>
         {t("par1")}
