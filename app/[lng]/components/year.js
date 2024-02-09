de4ce904acc2e0abb19590a11a0105801ba051b1
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-09 16:36:18 +0100

    year counter

---
+++ app/[lng]/components/year.js
@@ -0,0 +1,4 @@
+export default function YearCounter() {
+  const getYear = new Date().getFullYear() - 2019;
+  return getYear;
+}
--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -1,12 +1,18 @@
 import { useTranslation } from "../i18n";
+import YearCounter from "./components/year";
 
 export default async function Home({ params: { lng } }) {
   const { t } = await useTranslation(lng);
+
   return (
     <main>
       <>
         <h1>{t("title")}</h1>
-        <p>{t("par1")}</p>
+        <p>
+          {t("par1")}
+          <YearCounter />
+          {t("par2")}
+        </p>
       </>
     </main>
   );
