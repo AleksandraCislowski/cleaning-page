e03f8ba77484e402734d1348ccfeb731e1366101
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-09 22:02:38 +0100

    wave2

--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -2,14 +2,15 @@ import { useTranslation } from "../i18n";
 import { FirmaYearCounter } from "./components/year-counter";
 import { PersonYearCounter } from "./components/year-counter";
 import Hero from "./components/hero/hero";
+import classes from "./components/navigation/navigation.module.css";
 
 export default async function Home({ params: { lng } }) {
   const { t } = await useTranslation(lng);
 
   return (
     <main>
-      <link rel='icon' href='' sizes='any' />
       <Hero />
+      <div className={classes.bg2} />
       <h1>{t("title")}</h1>
       <p>
         {t("par1")}
