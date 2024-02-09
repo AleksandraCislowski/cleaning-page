e56cb1c03e05a7d337d4d96119bb4d0d4ec7e0b0
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-09 14:17:09 +0100

    language switcher

---
+++ app/[lng]/layout.js
@@ -0,0 +1,24 @@
+import { dir } from "i18next";
+import { languages } from "../i18n/settings";
+import Navigation from "../components/navigation/navigation";
+import "../globals.css";
+
+export const metadata = {
+  title: "Aleksandra Cislowski Städfirma",
+  description: "Cleaning services in Stockholm",
+};
+
+export async function generateStaticParams() {
+  return languages.map((lng) => ({ lng }));
+}
+
+export default function RootLayout({ children, params: { lng } }) {
+  return (
+    <html lang={lng} dir={dir(lng)}>
+      <body>
+        <Navigation />
+        {children}
+      </body>
+    </html>
+  );
+}
--- app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>eufhewifghe</h1>
-    </main>
-  );
-}
