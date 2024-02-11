425ead8904b70804bf04597d5d78fc82845ac6fa
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-11 15:48:03 +0100

    styling other cards

--- app/[lng]/card1/page.js
+++ app/[lng]/card1/page.js
@@ -1,13 +1,17 @@
 import { useTranslation } from "@/app/i18n";
-import { usePathname } from "next/navigation";
+import classes from "./page.module.css";
 
 export default async function Card1SubPage({ params: { lng } }) {
   const { t } = await useTranslation(lng);
 
   return (
     <div>
-      <h1>{t("title")}</h1>
-      <p>1</p>
+      <div className={classes.hero}></div>
+      <div className={classes.container}>
+        <h1 className={classes.header1}>{t("c1h1")}</h1>
+        <h2 className={classes.header2}>{t("c1h2")}</h2>
+        <p className={classes.paragraph}>{t("c1p1")}</p>
+      </div>
     </div>
   );
 }
