ca0e4dd733036dc0947b428409a608414ad6c785
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-12 00:28:32 +0100

    adding content to the att1

--- app/[lng]/card1/attachment1/bilaga1.js
+++ app/[lng]/card1/attachment1/bilaga1.js
@@ -16,6 +16,20 @@ export default async function Bilaga1({ lng }) {
           <p className={classes.paragraph}>{t("att1p4")}</p>
           <p className={classes.paragraph}>{t("att1p5")}</p>
           <p className={classes.paragraph}>{t("att1p6")}</p>
+          <p className={classes.paragraph2}>{t("att1p7")}</p>
+          <ul className={classes.list}>
+            <li>{t("att1li1")}</li>
+            <li>{t("att1li2")}</li>
+            <li>{t("att1li3")}</li>
+            <li>{t("att1li4")}</li>
+            <li>{t("att1li5")}</li>
+            <li>{t("att1li6")}</li>
+            <li>{t("att1li7")}</li>
+            <li>{t("att1li8")}</li>
+            <li>{t("att1li9")}</li>
+            <li>{t("att1li10")}</li>
+            <li>{t("att1li11")}</li>
+          </ul>
         </div>
       </div>
       <section className={classes.spikes2}></section>
