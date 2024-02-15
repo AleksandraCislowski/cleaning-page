498bd22d7b0204c1abab65d8475b94a5fcdff809
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-15 22:17:40 +0100

    s7 texts

--- app/[lng]/components/section7/card.js
+++ app/[lng]/components/section7/card.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import classes from "./section7.module.css";
-export default function Card() {
+export default function Card({ icon, text, text2 }) {
   return (
     <div className={classes.card}>
       <div>
-        <div>IKONA</div>
-        <p>TEKST</p>
-        <Link href=''>FB</Link>
+        <div>{icon}</div>
+        <p>{text}</p>
+        <p>{text2}</p>
       </div>
     </div>
   );
--- app/[lng]/components/section7/section7.js
+++ app/[lng]/components/section7/section7.js
@@ -3,21 +3,23 @@ import Card from "./card";
 import Image from "next/image";
 import logo from "@/assets/icons/logo.png";
 import Link from "next/link";
+import { useTranslation } from "@/app/i18n";
 
-export default function SectionSeven({ lng }) {
+export default async function SectionSeven({ lng }) {
+  const { t } = await useTranslation(lng);
   return (
     <div className={classes.container}>
       <div className={classes.box}>
-        <h1 className={classes.header}>Kontakta</h1>
+        <h1 className={classes.header}>{t("s7h")}</h1>
         <Link href={`/${lng}`}>
           <Image src={logo} className={classes.icon} />
         </Link>
       </div>
       <div className={classes.box}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card icon='' text={t("s7p1")} text2='073 333 89 01' />
+        <Card icon='' text={t("s7p2")} text2='aleksandra.swigon91@gmail.com' />
+        <Card icon='' text={t("s7p3")} text2={t("s7p4")} />
+        <Card icon='' text={t("s7p5")} />
       </div>
     </div>
   );
